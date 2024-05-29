149f05a54884a543738c467403fbd4fd27a51767
PhuNguyenNg <ngocphu.nguyen@techsoft.asia>
2024-05-29 05:28:28 -0400

    Create SignOut, ForgotPassword Apis

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using english_learning_server.Dtos.Account;
 using english_learning_server.Interfaces;
 using english_learning_server.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,25 +12,22 @@ namespace english_learning_server.Controllers
     [Route("api/users")]
     public class AccountController : ControllerBase
     {
-        private readonly ILogger<AccountController> _logger;
         private readonly UserManager<User> _userManager;
-
         private readonly ITokenService _tokenService;
-
+        private readonly IEmailService _emailService;
         private readonly SignInManager<User> _signInManager;
-
         private readonly IProfileRepository _profileRepo;
 
-        public AccountController(ILogger<AccountController> logger, UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager, IProfileRepository profileRepo)
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, IEmailService emailService, SignInManager<User> signInManager, IProfileRepository profileRepo)
         {   
-            _logger = logger;
             _userManager = userManager;
             _tokenService = tokenService;
+            _emailService = emailService;
             _signInManager = signInManager;
             _profileRepo = profileRepo;
         }
 
-        [HttpPost("login")]
+        [HttpPost("signIn")]
         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
         {
             try
@@ -119,6 +117,96 @@ namespace english_learning_server.Controllers
             catch (Exception e)
             {
                 return StatusCode(500, new { message = e.Message });
+            }
+        }
+
+        [HttpPost("signOut")]
+        [Authorize]
+        public async Task<IActionResult> LogOut() {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return Unauthorized("No user is currently logged in");
+            }
+
+            await _signInManager.SignOutAsync();
+
+            return Ok(
+                new
+                {
+                    success = true,
+                    message = "Profile updated successfully",
+                }
+            );
+        }
+
+        [HttpPost("forgotPassword")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto forgotPasswordDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
+
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+            {
+                return NotFound("User not found");
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            var callbackUrl = Url.Action("ResetPassword", "Account", new { token, email = user.Email }, Request.Scheme);
+
+            await _emailService.SendEmailAsync(user.Email, "Reset Password",
+                $"Please reset your password by <a href='{callbackUrl}'>clicking here</a>");
+
+            return Ok(
+                new
+                {
+                    success = true,
+                    message = "Email sent successfully",
+                }
+            );
+        }
+
+        [HttpPost("resetPassword")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(resetPasswordDto.Email);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, resetPasswordDto.Token, resetPasswordDto.NewPassword);
+
+            if (result.Succeeded)
+            {
+                return Ok(
+                    new
+                    {
+                        success = true,
+                        message = "Password reset successfully",
+                    }
+                );
+            }
+            else
+            {
+                return StatusCode(500, result.Errors);
             }
         }
     }
